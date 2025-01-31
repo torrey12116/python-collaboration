d3c8cf0921f8327a64b3d64a85d6cbb6d31d83e8
torrey12116 <tiwinrow@gmail.com>
2025-01-31 12:58:47 -0500

    This is my first file

---
+++ ExampleNestedForloops.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 12:17:14 2025
+
+@author: Torrey
+"""
+
+A = [ [30, 100, 101],
+     [40, 3, 2],
+     [7, 200, 50]]
+
+for row in A:
+    for i in row:
+        print(i, end=" ")
+    print()
+#----------------------------------------------------    
+for Myindex_R, row in enumerate(A):
+    for Myindex_C, Col in enumerate(row):
+        print(f" Index: {Myindex_R    }, {Myindex_C)={Col}")
+#----------------------------------------------------
+X=200
+Y='Python
+print(X+Y)
+           
